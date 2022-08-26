2c0b7ff185356e3ec0879b176ee782965b70d4c2
Dan-Santarossa <dpsantarossa@gmail.com>
2022-08-26 14:38:47 -0400

    added open access port

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 module "network" {
   source        = "./network"
   vpc_cidr      = "10.0.0.0/16"
-  access_ip     = var.access_ip
+  access_ip     = "0.0.0.0/0"
   public_cidrs  = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
   private_cidrs = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
 }
