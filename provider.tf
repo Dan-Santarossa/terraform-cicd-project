1b144eeb11b857dc4f2f8a2569dbfefdca36ce5d
Dan-Santarossa <dpsantarossa@gmail.com>
2022-08-27 00:50:04 -0400

    provider test 2

--- provider.tf
+++ provider.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 3.27"
     }
   }
 
-  required_version = "~> 1.2"
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
